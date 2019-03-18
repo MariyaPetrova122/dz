9b93721d08d34e5bb44d373ed84b524886626927
MariyaPetrova122 <MariyaPetrova122@users.noreply.github.com>
2019-03-19 00:08:58 +0300

    Update meladze_bot.py

--- meladze_bot/meladze_bot.py
+++ meladze_bot/meladze_bot.py
@@ -49,6 +49,28 @@ markup3 = ReplyKeyboardMarkup().add(
 @dp.message_handler(commands=['hi3'])
 async def process_hi3_command(message: types.Message):
     await message.reply("Третье - добавляем больше кнопок", reply_markup=kb.markup3)
+			 
+@dp.callback_query_handler(func=lambda c: c.data == 'button1')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+    chat_handler = CommandHandler('talk', lets_talk, pass_args=True)
+    updater.dispatcher.add_handler(chat_handler)
+			 
+@send_typing_action
+def lets_talk(bot, update):
+    args = input("Entre a phrase: ")
+    with open('texts.txt', 'r') as data:
+        data = data.read().split('\n')
+        model= Doc2Vec.load("d2v.model")
+        #to find the vector of a document which is not in training data
+        test_data = [w for w in word_tokenize(' '.join(args).lower()) if w.isalnum()]
+        v1 = model.infer_vector(test_data)
+        similar_doc = model.docvecs.most_similar([v1])
+        bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+        bot.send_message(chat_id=update.message.chat_id, text=data[int(similar_doc[0][0])])
+
+
 
     
 # пустая главная страничка для проверки
